365a4e7f3a1d529cbcc89e8f9d4c24935b506529
RoBlockHead <15642007+RoBlockHead@users.noreply.github.com>
2021-10-07 00:14:13 -0400

    fix it so it works lol

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-export default ()=>{return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,(c)=>{var r=Math.random()*16|0,v=c=='x'?r:(r&0x3|0x8);return v.toString(16)})}
+module.exports=()=>{return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,(c)=>{var r=Math.random()*16|0,v=c=='x'?r:(r&0x3|0x8);return v.toString(16)})}
